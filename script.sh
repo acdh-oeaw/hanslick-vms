e8983f72ff0c10c67a32f53c510eaf02e4811f7e
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2022-04-29 12:51:11 +0200

    updated workflow

--- script.sh
+++ script.sh
@@ -3,6 +3,4 @@
 echo "downloading saxon"
 wget https://sourceforge.net/projects/saxon/files/Saxon-HE/9.9/SaxonHE9-9-1-7J.zip/download && unzip download -d saxon && rm -rf download
 echo "downloading TEIC stylesheets"
-wget https://github.com/TEIC/Stylesheets/archive/refs/heads/dev.zip
-unzip dev
-rm dev.zip
+wget https://github.com/TEIC/Stylesheets/archive/refs/heads/dev.zip && unzip dev.zip -d TEI-Stylesheets && rm -rf dev.zip
